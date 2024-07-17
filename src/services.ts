efd983b3cdcf5e32cfb4aa87cef57ff1fee7f048
Ecliptek-exe <dorsettj@gmail.com>
2024-07-16 22:30:36 -0700

    implemented s3 functionality

--- src/services.ts
+++ src/services.ts
@@ -1,14 +1,15 @@
-import { openPage, screenshotElement, storePage } from "./utils";
-
+import { openPage, screenshotElement } from "./utils";
+import { uploadFileToS3 } from "./utils/s3Tools";
 export const extractChapter = async (mangaId: string, chapterId: string) => {
   const page = await openPage(`https://chapmanganato.to/manga-${mangaId}/chapter-${chapterId}`);
 
-  const chapterUrlsPromises = (await page.$$(".container-chapter-reader img")).map(async (element, index) => {
+  const chapterUrlsPromises = (await page.$$(".container-chapter-reader img"))
+    .map(async (element, index) => {
         const screenshot = await screenshotElement(element, page);
         if (screenshot) {
-            // await storePage(mangaId, chapterId, index, screenshot);
+          await uploadFileToS3(`manga-${mangaId}/chapter-${chapterId}/page-${index}.jpg`, screenshot);
         }
-        return `/manga-${mangaId}/chapter-${chapterId}/page-${index}`;
+        return `manga-${mangaId}/chapter-${chapterId}/page-${index}`;
   });
 
   return await Promise.all(chapterUrlsPromises);
